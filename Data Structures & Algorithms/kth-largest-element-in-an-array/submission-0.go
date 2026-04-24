fcd7931468be60f2e57371112a41039930d3b0d2
Ian Mwangi <62850662+yaninyzwitty@users.noreply.github.com>
2026-04-24 21:55:38 +0300

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.go
@@ -0,0 +1,29 @@
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	val := old[n-1]
+	*h = old[:n-1]
+	return val
+}
+
+func findKthLargest(nums []int, k int) int {
+	h := &MinHeap{}
+	heap.Init(h)
+	for _, n := range nums {
+			heap.Push(h, n)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	return (*h)[0]
+}
